d02b3ed47a3766d7ca7a53b96983de9fabe7f1e1
张攀钦 <zhangpanqin>
2019-08-01 18:52:27 +0800

    feat:提交

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-// import "@babel/polyfill";
 import _ from "lodash";
-function component() {
-  var element = document.createElement("div");
+function component () {
+    var element = document.createElement("div");
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  return element;
+    return element;
 }
 
 document.body.appendChild(component());
 
 class Demo {
-  log() {
-    console.log("index");
-  }
+    log () {
+        console.log("index");
+    }
 }
 new Demo().log();
 Promise.resolve("index").then(data => console.log(data));
